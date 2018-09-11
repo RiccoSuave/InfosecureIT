b680b0e4835fbbb0845fec7f8ac5dc1eef23bc74
Sean <shansari@verizon.net>
2018-09-11 11:16:18 -0400

    first commit

---
+++ InfosecureIT/Controllers/ClientInquiriesController.cs
@@ -0,0 +1,26 @@
+﻿using InfosecureIT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InfosecureIT.Controllers
+{
+    public class ClientInquiriesController : Controller
+    {
+        // GET: ClientInquiries
+        public ActionResult Index()
+        {
+            List<Client> NewClientInquirie = new List<Client>();
+            Client firstClient = new Client();
+            firstClient.FirstName = "Shahin";
+            firstClient.LastName = "Ansari";
+            firstClient.LastName = "571-549-1026";
+            firstClient.Title = "Director, CEO";
+            firstClient.PotentialProject = "We like to improve the security of our Web Services";
+            NewClientInquirie.Add(firstClient);
+            return View(NewClientInquirie);
+        }
+    }
+}
---
+++ InfosecureIT/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InfosecureIT.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Our activities and services are as follows:";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "To find out more...";
+
+            return View();
+        }
+    }
+}
